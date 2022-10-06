1815b736e765401f759fe1558a27149a3b95e13e
Shaldy <potasofka@gmail.com>
2022-10-06 16:30:09 +0500

    added ready map files

---
+++ OrenMap/Contents/mods/OrenMap/media/maps/OrenMap/objects.lua
@@ -0,0 +1,5 @@
+objects = {
+  { name = "", type = "ParkingStall", x = 130, y = 108, z = 0, width = 4, height = 7 },
+  { name = "", type = "ParkingStall", x = 149, y = 96, z = 0, width = 10, height = 18 },
+  { name = "", type = "SpawnPoint", x = 132, y = 117, z = 0, width = 1, height = 1, properties = { Professions = "all" } }
+}
